a881d81c78e511c92edc118ab7575dd0a3d5b12c
alexdtat <alexdtat@gmail.com>
2022-07-12 23:29:16 +0300

    Raw interface for android application.

---
+++ app/src/main/java/me/alexdtat/featuredetectandroid/MainActivity.kt
@@ -0,0 +1,27 @@
+package me.alexdtat.featuredetectandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import me.alexdtat.featuredetectandroid.ui.theme.Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Theme {
+                Box(Modifier.fillMaxSize()) {
+                    FloatingActionButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    ) { }
+                }
+            }
+        }
+    }
+}
