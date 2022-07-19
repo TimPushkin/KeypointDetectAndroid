174fc8e36ee904fce75f744fcc557ab382f3aa34
paul <rv4p@ya.ru>
2022-07-19 20:38:00 +0300

    Add SIFT descriptor measure func

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,4 +1,4 @@
-#include "../include/featurelib.h"
+#include "../include/FeatureLib.h"
 #include <vector>
 #include <iostream>
 #include <cstdint>
@@ -14,14 +14,23 @@ std::vector<uint8_t> matToVector(const cv::Mat &mat) {
 int main() {
   std::string image_path = cv::samples::findFile("church.jpg");
   cv::Mat img = imread(image_path, cv::IMREAD_GRAYSCALE);
-  std::vector<uint8_t> testVector= matToVector(img);
+  std::vector<uint8_t> testVector = matToVector(img);
+
   int width = img.cols;
   int height = img.rows;
-  std::vector<libstructs::KeyPoint> testVectorOfKeyPoints;
-  featurelib featurelib(testVector, width, height);
-  testVectorOfKeyPoints = featurelib.getSiftKeyPoints();
-  for (auto &kp : testVectorOfKeyPoints) {
-    std::cout << kp.x<<"\t"<<kp.y<<std::endl ;
+
+  FeatureLib featurelib(testVector, width, height);
+
+  std::vector<libstructs::KeyPoint> testVectorOfKeyPoints = featurelib.getSiftKeyPoints();
+  std::vector<uint8_t> testVectorOfDescriptors = featurelib.getSiftDescriptors();
+  std::cout << "Printing points" << std::endl;
+  for (auto &tempKp : testVectorOfKeyPoints) {
+    std::cout << tempKp.x << "\t" << tempKp.y << std::endl;
+  }
+  std::cout << "Printing descriptors" << std::endl;
+
+  for (auto &tempUint : testVectorOfDescriptors) {
+    std::cout << tempUint << std::endl;
   }
   return 0;
 }
