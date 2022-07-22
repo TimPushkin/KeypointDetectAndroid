e77910961503e560c8a48a4210df2eba0c5f7226
TimPushkin <pushkin.td@gmail.com>
2022-07-22 11:00:05 +0300

    Add lib module with SuperPoint

---
+++ lib/src/main/java/com/github/featuredetectlib/FeatureDetector.kt
@@ -0,0 +1,29 @@
+package com.github.featuredetectlib
+
+typealias Descriptor = List<Float>
+
+/**
+ * Detects keypoints and their descriptors on an image.
+ */
+interface FeatureDetector {
+    /**
+     * Width of the images to be processed.
+     */
+    var width: Int
+
+    /**
+     * Height of the passed to be processed.
+     */
+    var height: Int
+
+    /**
+     * Detects keypoints and their descriptors on an image.
+     *
+     * Returns keypoints and descriptors so that elements with the same index correspond to each
+     * other.
+     *
+     * @param image array of image information where each pixel is represented as three successive
+     * R, G, B bytes in the order in which the pixels appear in the image.
+     */
+    fun detect(image: ByteArray): Pair<List<KeyPoint>, List<Descriptor>>
+}
---
+++ lib/src/main/java/com/github/featuredetectlib/SuperPoint.kt
@@ -0,0 +1,90 @@
+package com.github.featuredetectlib
+
+import android.content.Context
+import org.pytorch.IValue
+import org.pytorch.LiteModuleLoader
+import org.pytorch.Tensor
+import kotlin.math.pow
+import kotlin.properties.Delegates
+
+private const val NETWORK_ASSET = "superpoint.ptl"
+private const val DECODER_ASSET = "superpoint_decoder.ptl"
+
+private const val DESCRIPTOR_SIZE = 256
+
+/**
+ * Detects learned keypoints and their descriptors using
+ * [SuperPoint](https://github.com/magicleap/SuperPointPretrainedNetwork) network.
+ */
+class SuperPoint(context: Context, width: Int, height: Int) : FeatureDetector {
+    private val net = LiteModuleLoader.loadModuleFromAsset(context.assets, NETWORK_ASSET)
+    private val decoder = LiteModuleLoader.loadModuleFromAsset(context.assets, DECODER_ASSET)
+
+    private var longWidth by Delegates.notNull<Long>()
+    private lateinit var wrappedWidth: IValue
+    override var width: Int = width
+        set(value) {
+            field = value
+            longWidth = value.toLong()
+            wrappedWidth = IValue.from(longWidth)
+        }
+
+    private var longHeight by Delegates.notNull<Long>()
+    private lateinit var wrappedHeight: IValue
+    override var height: Int = height
+        set(value) {
+            field = value
+            longHeight = value.toLong()
+            wrappedHeight = IValue.from(longHeight)
+        }
+
+    override fun detect(image: ByteArray): Pair<List<KeyPoint>, List<Descriptor>> {
+        val input = Tensor.fromBlob(rgbToGrayscale(image), longArrayOf(1, 1, longHeight, longWidth))
+        val output = net.forward(IValue.from(input)).toTuple()
+        val decoded = decoder.forward(output[0], output[1], wrappedHeight, wrappedWidth).toTuple()
+
+        val keyPoints = keyPointTensorToKeyPointList(decoded[0].toTensor())
+        val descriptors = descriptorTensorToDescriptorList(decoded[1].toTensor())
+
+        return keyPoints to descriptors
+    }
+
+    @Suppress("MagicNumber")
+    private fun rgbToGrayscale(pixels: ByteArray): FloatArray =
+        FloatArray((width / 3) * height) { i ->
+            val r = linearizeSrgbChannel((pixels[i] + 128) / 255f)
+            val g = linearizeSrgbChannel((pixels[i + 1] + 128) / 255f)
+            val b = linearizeSrgbChannel((pixels[i + 2] + 128) / 255f)
+            return@FloatArray 0.2126f * r + 0.7152f * g + 0.0722f * b
+        }
+
+    @Suppress("MagicNumber")
+    private fun linearizeSrgbChannel(value: Float): Float =
+        if (value <= 0.04045) value / 12.92f else ((value + 0.055f) / 1.055f).pow(2.4f)
+
+    private fun keyPointTensorToKeyPointList(tensor: Tensor): List<KeyPoint> {
+        val data = tensor.dataAsFloatArray
+        val keyPointsNum = tensor.shape()[1].toInt()
+        val list =
+            List(keyPointsNum) { i ->
+                KeyPoint(
+                    x = data[i],
+                    y = data[keyPointsNum + i],
+                    strength = data[2 * keyPointsNum + i]
+                )
+            }
+        return list
+    }
+
+    private fun descriptorTensorToDescriptorList(tensor: Tensor): List<Descriptor> {
+        val data = tensor.dataAsFloatArray
+        val descriptorsNum = tensor.shape()[1].toInt()
+        val list =
+            List(descriptorsNum) { j ->
+                List(DESCRIPTOR_SIZE) { i ->
+                    data[DESCRIPTOR_SIZE * i + j]
+                }
+            }
+        return list
+    }
+}
