13dabf3c6e034cc7ee368c5b79471b6a645152e0
paul <rv4p@ya.ru>
2022-07-27 00:21:21 +0300

    ISSUE print descriptors smarted done ISSUE use cli algo which pick algo for detection done

--- lib/src/main/cpp/examples/detection_example.cpp
+++ lib/src/main/cpp/examples/detection_example.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include "feature_lib.h"
+
+template<typename Iterator>
+void advance_all(Iterator &iterator) {
+  ++iterator;
+}
+template<typename Iterator, typename ... Iterators>
+void advance_all(Iterator &iterator, Iterators &... iterators) {
+  ++iterator;
+  advance_all(iterators...);
+}
+template<typename Function, typename Iterator, typename ... Iterators>
+Function zip(Function func, Iterator begin,
+             Iterator end,
+             Iterators ... iterators) {
+  for (; begin != end; ++begin, advance_all(iterators...))
+    func(*begin, *(iterators)...);
+  //could also make this a tuple
+  return func;
+}
 
 std::vector<uint8_t> matToVector(const cv::Mat &mat) {
   std::vector<uint8_t> result;
@@ -10,48 +31,66 @@ std::vector<uint8_t> matToVector(const cv::Mat &mat) {
   return result;
 }
 
-void printOutput(const featurelib::DetectionResult &output) {
-  std::cout << "Printing points" << std::endl;
-  for (auto &kp : output.keypoints) {
-    std::cout << kp.x << "\t" << kp.y << "\t" << kp.size << "\t" << kp.angle << "\t" << kp.strength << std::endl;
-  }
+void printOutput(const featurelib::DetectionResult &output, std::string &algorithm) {
+  std::cout << algorithm << " keypoints and size of associated descriptors arrays" << std::endl;
+  zip(
+      [](featurelib::KeyPoint kp, std::vector<uint8_t> row) {
 
-  std::cout << "Printing descriptors" << std::endl;
-  for (const auto &row : output.descriptors) {
-    for (const auto &element : row) std::cout << element << ' ';
-    std::cout << std::endl;
-  }
+        std::cout << kp.x << "\t" << kp.y << "\t" << kp.size << "\t" << kp.angle << "\t" << kp.strength << "\t"
+                  << row.size() << std::endl;
+      },
+      output.keypoints.begin(), output.keypoints.end(), output.descriptors.begin());
 }
 
 // Calculates key points and image descriptors
 // Takes an image file path as input
 // Prints key points and image descriptors to stdout
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Illegal CLI arguments num. Expected only a file path but was " << argc
+  std::map<std::string, int> hash{{"wrong", 0}, {"sift", 1}, {"surf", 2}, {"orb", 3}};
+
+  if (argc != 3) {
+    std::cerr << "Illegal CLI arguments num. Expected file path and detector name but was " << argc
               << " arguments (including the command name)" << std::endl;
     return 1;
   }
   auto file_path = argv[1];
-
+  std::string algorithm = argv[2];
+  std::transform(algorithm.begin(), algorithm.end(), algorithm.begin(), ::tolower);
+  try {
+    if (!hash.count(algorithm))
+      throw "No algo matching try again with std::cin:";
+  }
+  catch (const char *exception) {
+    std::cerr << "Error: " << exception << '\n';
+    std::cin >> algorithm;
+  }
   cv::Mat img = imread(file_path, cv::IMREAD_COLOR);
   auto width = img.cols;
   auto height = img.rows;
-
+  featurelib::DetectionResult output;
   std::vector<uint8_t> image_data = matToVector(img);
-
-  featurelib::SiftDetector sift_detector(width, height);
-  featurelib::DetectionResult sift_output = sift_detector.detect(image_data);
-
-  featurelib::OrbDetector orb_detector(width, height);
-  featurelib::DetectionResult orb_output = orb_detector.detect(image_data);
-
-  featurelib::SurfDetector surf_detector(width, height);
-  featurelib::DetectionResult surf_output = surf_detector.detect(image_data);
-
-  printOutput(sift_output);
-  printOutput(orb_output);
-  printOutput(surf_output);
+  switch (hash[algorithm]) {
+    case 1: {
+      featurelib::SiftDetector sift_detector(width, height);
+      output = sift_detector.detect(image_data);
+      break;
+    }
+    case 2: {
+      featurelib::SurfDetector surf_detector(width, height);
+      output = surf_detector.detect(image_data);
+      break;
+    }
+    case 3: {
+      featurelib::OrbDetector orb_detector(width, height);
+      output = orb_detector.detect(image_data);
+      break;
+    }
+    default: {
+      std::cout << "wrong algorithm name" << std::endl;
+      break;
+    }
+  }
+  printOutput(output, algorithm);
 
   return 0;
 }
