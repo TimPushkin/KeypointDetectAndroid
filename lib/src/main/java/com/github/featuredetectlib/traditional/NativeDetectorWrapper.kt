e74a989caf7d42617c90667e83cd1dfd069f02b6
TimPushkin <pushkin.td@gmail.com>
2022-07-27 16:51:40 +0300

    Fix size setting in native detector wrapper

--- lib/src/main/java/com/github/featuredetectlib/traditional/NativeDetectorWrapper.kt
+++ lib/src/main/java/com/github/featuredetectlib/traditional/NativeDetectorWrapper.kt
@@ -9,8 +9,16 @@ import com.github.featuredetectlib.traditional.FeatureDetector as NativeDetector
  * Wrapper for a native feature detector.
  */
 internal class NativeDetectorWrapper(private val detector: NativeDetector) : FeatureDetector {
-    override var width = detector.width
-    override var height = detector.height
+    override var width: Int
+        get() = detector.width
+        set(value) {
+            detector.width = value
+        }
+    override var height: Int
+        get() = detector.height
+        set(value) {
+            detector.height = value
+        }
 
     override fun detect(image: ByteArray): Pair<List<Keypoint>, List<Descriptor>> {
         val output = detector.detect(image)
