1f0de67b6a72b8f2ae3edfa5440e5fa4a70393ee
TimPushkin <pushkin.td@gmail.com>
2022-07-28 10:52:35 +0300

    Make image fullscreen

--- app/src/main/java/com/github/featuredetectandroid/ui/AppLayout.kt
+++ app/src/main/java/com/github/featuredetectandroid/ui/AppLayout.kt
@@ -1,22 +1,21 @@
 package com.github.featuredetectandroid.ui
 
 import android.graphics.Bitmap
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalDensity
 import com.github.featuredetectandroid.utils.KeypointDetectionAlgorithm
 
 @Composable
@@ -44,28 +43,26 @@ fun AppLayout(
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
-            if (isCameraPermissionGranted) {
-                frameBitmap?.let { bitmap ->
-                    Image(
-                        bitmap = bitmap.asImageBitmap(),
-                        contentDescription = "Grayscale photo"
-                    )
-
-                    val (width, height) = with(LocalDensity.current) {
-                        bitmap.width.toDp() to bitmap.height.toDp()
-                    }
-
-                    Canvas(modifier = Modifier.size(width, height)) {
-                        drawPoints(
-                            points = keypointOffsets,
-                            pointMode = PointMode.Points,
-                            color = Color.Blue,
-                            strokeWidth = 10f
-                        )
-                    }
-                }
-            } else {
+            if (!isCameraPermissionGranted) {
                 Text("Camera permission required")
+                return@Box
+            }
+
+            frameBitmap?.asImageBitmap()?.let { bitmap ->
+                Canvas(bitmap).drawPoints(
+                    pointMode = PointMode.Points,
+                    points = keypointOffsets,
+                    paint = Paint().apply {
+                        color = Color.Blue
+                        strokeWidth = 10f
+                    }
+                )
+
+                Image(
+                    bitmap = bitmap,
+                    contentDescription = "Grayscale photo",
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
