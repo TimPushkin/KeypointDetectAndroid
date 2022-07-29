22e97c5142817b86d9444026be8ecd934129461d
TimPushkin <pushkin.td@gmail.com>
2022-07-29 10:53:27 +0300

    Fix RGB conversions naming

--- app/src/main/java/com/github/featuredetectandroid/utils/PhotoAnalyzer.kt
+++ app/src/main/java/com/github/featuredetectandroid/utils/PhotoAnalyzer.kt
@@ -10,7 +10,7 @@ import java.nio.ByteBuffer
 
 private const val TAG = "PhotoAnalyzer"
 private const val ROTATION_STEP = 90
-private const val RGBA_COMPONENTS = 4
+private const val RGBA_COMPONENTS_NUM = 4
 
 class PhotoAnalyzer(private val outputViewModel: OutputViewModel) : ImageAnalysis.Analyzer {
     private fun ByteBuffer.toByteArray(): ByteArray {
@@ -19,16 +19,16 @@ class PhotoAnalyzer(private val outputViewModel: OutputViewModel) : ImageAnalysi
     }
 
     private fun rotateClockwiseOnRotationStep(
-        grayscaleByteArray: ByteArray,
+        imageArray: ByteArray,
         width: Int,
         height: Int
-    ) = ByteArray(RGBA_COMPONENTS * width * height).also { array ->
+    ) = ByteArray(RGBA_COMPONENTS_NUM * width * height).also { array ->
         for (i in 0 until width) {
             for (j in 0 until height) {
-                for (componentIndex in 0 until RGBA_COMPONENTS) {
-                    array[RGBA_COMPONENTS * (i * height + j) + componentIndex] =
-                        grayscaleByteArray[
-                            RGBA_COMPONENTS * (i + (height - 1 - j) * width) + componentIndex
+                for (componentIndex in 0 until RGBA_COMPONENTS_NUM) {
+                    array[RGBA_COMPONENTS_NUM * (i * height + j) + componentIndex] =
+                        imageArray[
+                            RGBA_COMPONENTS_NUM * (i + (height - 1 - j) * width) + componentIndex
                         ]
                 }
             }
@@ -38,16 +38,12 @@ class PhotoAnalyzer(private val outputViewModel: OutputViewModel) : ImageAnalysi
     override fun analyze(image: ImageProxy) {
         var width = image.width
         var height = image.height
-        var oriented = image
-            .planes[0]
-            .buffer
-            .toByteArray()
+        var oriented = image.planes[0].buffer.toByteArray()
 
         // Rotation of incorrectly oriented images is implemented here.
-        // TODO: Optimize rotations for 180 and 270 degrees.
         val rotationDegrees = image.imageInfo.rotationDegrees
         repeat(rotationDegrees / ROTATION_STEP) {
-            Log.i(TAG, "The image is rotated on $rotationDegrees degrees.")
+            Log.v(TAG, "The image is rotated on $rotationDegrees degrees.")
             oriented = rotateClockwiseOnRotationStep(oriented, width, height)
             width = height.also { height = width }
         }
@@ -56,7 +52,7 @@ class PhotoAnalyzer(private val outputViewModel: OutputViewModel) : ImageAnalysi
             outputViewModel.featureDetector?.let { detector ->
                 if (detector.width != width) detector.width = width
                 if (detector.height != height) detector.height = height
-                val rgbArray = rgbaComponentsToRGBByteArray(oriented)
+                val rgbArray = rgbaComponentsToRgbByteArray(oriented)
                 val startTime = SystemClock.elapsedRealtime()
                 val (keypoints, _) = detector.detect(rgbArray)
                 outputViewModel.calcTimeMs = SystemClock.elapsedRealtime() - startTime
---
+++ app/src/main/java/com/github/featuredetectandroid/utils/RgbConversions.kt
@@ -0,0 +1,66 @@
+package com.github.featuredetectandroid.utils
+
+import android.graphics.Bitmap
+import java.nio.IntBuffer
+
+private const val RGB_COMPONENTS_NUM = 3
+private const val RGBA_COMPONENTS_NUM = 4
+
+/**
+ * Converts an array of RGBA pixel data into an array of RGB pixel data.
+ *
+ * @param imageArray RGBA pixels in format `[R_1, G_1, B_1, A_1, ..., R_n, G_n, B_n, A_n]`.
+ * @return RGB pixels in format `[R_1, G_1, B_1, ..., R_n, G_n, B_n]`.
+ */
+fun rgbaComponentsToRgbByteArray(imageArray: ByteArray): ByteArray {
+    val pixelsNum = imageArray.size / RGBA_COMPONENTS_NUM
+    val rgbArray = ByteArray(pixelsNum * RGB_COMPONENTS_NUM)
+    for (pixelIndex in 0 until pixelsNum) {
+        for (componentIndex in 0 until RGB_COMPONENTS_NUM) {
+            rgbArray[RGB_COMPONENTS_NUM * pixelIndex + componentIndex] =
+                imageArray[RGBA_COMPONENTS_NUM * pixelIndex + componentIndex]
+        }
+    }
+    return rgbArray
+}
+
+private const val RED_POS = 0 // Red channel position in RGBA pixel array
+private const val GREEN_POS = 1 // Green channel position in RGBA pixel array
+private const val BLUE_POS = 2 // Blue channel position in RGBA pixel array
+private const val ALPHA_POS = 3 // Alpha channel position in RGBA pixel array
+
+private const val ALPHA_BIT_SHIFT = 24 // Alpha channel shift in ARGB_8888
+private const val BLUE_BIT_SHIFT = 16 // Blue channel shift in ARGB_8888
+private const val GREEN_BIT_SHIFT = 8 // Green channel shift in ARGB_8888
+private const val RED_BIT_SHIFT = 0 // Red channel shift in ARGB_8888
+
+private const val MAX_COLOR = 0xff // Maximum ARGB_8888 channel value
+
+/**
+ * Converts an array of RGBA pixel data into a [Bitmap] in [Bitmap.Config.ARGB_8888] format.
+ *
+ * @param imageArray RGBA pixels in format `[R_1, G_1, B_1, A_1, ..., R_n, G_n, B_n, A_n]`.
+ * @param width width of the image.
+ * @param height height of the image.
+ * @return mutable [Bitmap] in [Bitmap.Config.ARGB_8888] format containing the given pixels.
+ */
+fun rgbaComponentsToBitmap(imageArray: ByteArray, width: Int, height: Int): Bitmap {
+    val buffer = IntBuffer.allocate(imageArray.size / RGBA_COMPONENTS_NUM).apply {
+        for (pixelIndex in imageArray.indices step RGBA_COMPONENTS_NUM) {
+            val r = imageArray[pixelIndex + RED_POS].toInt() and MAX_COLOR
+            val g = imageArray[pixelIndex + GREEN_POS].toInt() and MAX_COLOR
+            val b = imageArray[pixelIndex + BLUE_POS].toInt() and MAX_COLOR
+            val a = imageArray[pixelIndex + ALPHA_POS].toInt() and MAX_COLOR
+            // ARGB_8888 pixel is stored in ABGR channel order
+            put(
+                (a shl ALPHA_BIT_SHIFT) or (b shl BLUE_BIT_SHIFT) or
+                        (g shl GREEN_BIT_SHIFT) or (r shl RED_BIT_SHIFT)
+            )
+        }
+        rewind()
+    }
+
+    return Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888).apply {
+        copyPixelsFromBuffer(buffer)
+    }
+}
