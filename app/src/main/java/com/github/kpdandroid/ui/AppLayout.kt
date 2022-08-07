b3d28e11db378060a10e36ef9dcfa1f04e886ab1
TimPushkin <pushkin.td@gmail.com>
2022-08-07 14:38:45 +0300

    Move algorithm selection into bottom bar

--- app/src/main/java/com/github/kpdandroid/ui/AppLayout.kt
+++ app/src/main/java/com/github/kpdandroid/ui/AppLayout.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,18 +26,11 @@ fun AppLayout(
     image: ImageBitmap?,
     calcTimeMs: Long?,
     isCameraPermissionGranted: Boolean,
-    selectedAlgorithmName: String,
+    initialAlgorithmName: String,
     onAlgorithmSelected: (String) -> Unit
 ) {
     Scaffold(
-        drawerContent = {
-            Menu(
-                header = "Keypoint detection algorithm:",
-                options = KeypointDetectionAlgorithm.names,
-                selectedOption = selectedAlgorithmName,
-                onSelected = onAlgorithmSelected
-            )
-        },
+        bottomBar = { BottomMenu(initialAlgorithmName, onAlgorithmSelected) },
         modifier = Modifier.fillMaxSize()
     ) { paddingValues ->
         if (!isCameraPermissionGranted) {
@@ -69,6 +68,29 @@ fun AppLayout(
                     modifier = Modifier.padding(30.dp)
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun BottomMenu(initialAlgorithmName: String, onAlgorithmSelected: (String) -> Unit) {
+    BottomAppBar {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BottomMenuItem(
+                options = KeypointDetectionAlgorithm.names,
+                initialOption = initialAlgorithmName,
+                onSelected = onAlgorithmSelected,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Build,
+                        contentDescription = "Detection algorithm"
+                    )
+                }
+            )
         }
     }
 }
