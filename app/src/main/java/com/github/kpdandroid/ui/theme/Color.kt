746617937228250e2c5fbf4b0d0486d8f6154cb3
TimPushkin <pushkin.td@gmail.com>
2022-08-07 11:35:23 +0300

    Add theme support

--- app/src/main/java/com/github/kpdandroid/ui/theme/Color.kt
+++ app/src/main/java/com/github/kpdandroid/ui/theme/Color.kt
@@ -2,12 +2,14 @@ package com.github.kpdandroid.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-private const val PURPLE_200 = 0xFFBB86FC
-private const val PURPLE_500 = 0xFF6200EE
-private const val PURPLE_700 = 0xFF3700B3
-private const val TEAL_200 = 0xFF03DAC5
+// Primary
+private const val BLUE_500 = 0xFF2196F3
+val Blue500 = Color(BLUE_500)
+private const val BLUE_500_LIGHT = 0xFF6EC6FF
+val Blue500Light = Color(BLUE_500_LIGHT)
+private const val BLUE_500_DARK = 0xFF0069C0
+val Blue500Dark = Color(BLUE_500_DARK)
 
-val Purple200 = Color(PURPLE_200)
-val Purple500 = Color(PURPLE_500)
-val Purple700 = Color(PURPLE_700)
-val Teal200 = Color(TEAL_200)
+// Secondary
+private const val CYAN_200 = 0xFF80DEEA
+val Cyan200 = Color(CYAN_200)
