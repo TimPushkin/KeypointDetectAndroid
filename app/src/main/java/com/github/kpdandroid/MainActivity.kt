3f05a020b3d745aeb4a26e94dad19e43b57a1077
TimPushkin <pushkin.td@gmail.com>
2022-08-08 13:30:41 +0300

    Implement resolution picking

--- app/src/main/java/com/github/kpdandroid/MainActivity.kt
+++ app/src/main/java/com/github/kpdandroid/MainActivity.kt
@@ -4,39 +4,41 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888
-import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Build
 import androidx.core.content.ContextCompat
 import com.github.kpdandroid.ui.AppLayout
+import com.github.kpdandroid.ui.BottomMenu
+import com.github.kpdandroid.ui.BottomMenuItem
 import com.github.kpdandroid.ui.SnapshotViewModel
 import com.github.kpdandroid.ui.theme.KeypointDetectAppTheme
+import com.github.kpdandroid.utils.CameraHandler
 import com.github.kpdandroid.utils.KeypointDetectionAlgorithm
-import com.github.kpdandroid.utils.PhotoAnalyzer
 import com.github.kpdandroid.utils.PreferencesManager
-import java.util.concurrent.Executors
+import com.github.kpdandroid.utils.SnapshotAnalyzer
 
 private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     private val snapshotViewModel by viewModels<SnapshotViewModel>()
     private lateinit var preferencesManager: PreferencesManager
-    private val cameraExecutor = Executors.newSingleThreadExecutor()
-    private var isAnalyzing = false
+    private lateinit var cameraHandler: CameraHandler
 
     private val cameraPermissionRequest =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
             if (granted) {
                 Toast.makeText(this, "Permissions granted.", Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "Permission was granted successfully.")
-                startCameraAnalysis()
+                startCameraAnalysisIfNeeded()
             } else {
                 Toast.makeText(this, "Permissions not granted.", Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "Permission was NOT granted.")
@@ -47,6 +49,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         preferencesManager = (application as KeypointDetectApp).preferencesManager
+        cameraHandler = CameraHandler(this, SnapshotAnalyzer(snapshotViewModel))
 
         snapshotViewModel.keypointDetector = KeypointDetectionAlgorithm.constructKeypointDetector(
             algorithmName = preferencesManager.selectedAlgorithmName,
@@ -63,16 +66,47 @@ class MainActivity : ComponentActivity() {
                     image = snapshotViewModel.paintedSnapshot,
                     calcTimeMs = snapshotViewModel.calcTimeMs,
                     isCameraPermissionGranted = isCameraPermissionGranted(),
-                    initialAlgorithmName = preferencesManager.selectedAlgorithmName,
-                    onAlgorithmSelected = { algorithmName ->
-                        preferencesManager.selectedAlgorithmName = algorithmName
-                        snapshotViewModel.keypointDetector =
-                            KeypointDetectionAlgorithm.constructKeypointDetector(
-                                algorithmName = algorithmName,
-                                context = this,
-                                width = snapshotViewModel.keypointDetector?.width ?: 0,
-                                height = snapshotViewModel.keypointDetector?.height ?: 0
+                    bottomMenu = {
+                        BottomMenu {
+                            BottomMenuItem(
+                                options = KeypointDetectionAlgorithm.names,
+                                selectedOption = preferencesManager.selectedAlgorithmName,
+                                onSelected = { algorithmName ->
+                                    preferencesManager.selectedAlgorithmName = algorithmName
+                                    snapshotViewModel.keypointDetector =
+                                        KeypointDetectionAlgorithm.constructKeypointDetector(
+                                            algorithmName = algorithmName,
+                                            context = this@MainActivity,
+                                            width = snapshotViewModel.keypointDetector?.width ?: 0,
+                                            height = snapshotViewModel.keypointDetector?.height ?: 0
+                                        )
+                                },
+                                leadingIcon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Build,
+                                        contentDescription = "Detection algorithm"
+                                    )
+                                }
                             )
+
+                            BottomMenuItem(
+                                options = cameraHandler.supportedResolutions.map { it.toString() },
+                                selectedOption = preferencesManager.selectedResolution?.toString()
+                                    ?: "Pick resolution",
+                                onSelected = { sizeString ->
+                                    cameraHandler.startImageAnalysis(
+                                        targetResolution = Size.parseSize(sizeString),
+                                        callback = preferencesManager::selectedResolution::set
+                                    )
+                                },
+                                leadingIcon = {
+                                    Icon(
+                                        imageVector = Icons.Default.AccountBox,
+                                        contentDescription = "Camera resolution"
+                                    )
+                                }
+                            )
+                        }
                     }
                 )
             }
@@ -85,15 +119,19 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onDestroy() {
-        cameraExecutor.shutdown()
+        cameraHandler.shutdown()
         super.onDestroy()
     }
 
     private fun startCameraAnalysisIfNeeded() {
-        if (isAnalyzing) return
+        if (cameraHandler.isAnalyzing) return
 
         when {
-            isCameraPermissionGranted() -> startCameraAnalysis()
+            isCameraPermissionGranted() ->
+                cameraHandler.startImageAnalysis(
+                    targetResolution = preferencesManager.selectedResolution,
+                    callback = preferencesManager::selectedResolution::set
+                )
             shouldRationalize() -> {
                 Toast.makeText(
                     this,
@@ -113,31 +151,4 @@ class MainActivity : ComponentActivity() {
     private fun shouldRationalize() =
         (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) &&
             shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
-
-    private fun startCameraAnalysis() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .setOutputImageFormat(OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                .setOutputImageRotationEnabled(true)
-                .build()
-                .apply { setAnalyzer(cameraExecutor, PhotoAnalyzer(snapshotViewModel)) }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            cameraProvider.unbindAll() // Just in case
-
-            try {
-                cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalyzer)
-                isAnalyzing = true
-            } catch (illegalState: IllegalStateException) {
-                Log.e(TAG, "Use case binding failed: ", illegalState)
-            } catch (illegalArgument: IllegalArgumentException) {
-                Log.e(TAG, "Use case binding failed: ", illegalArgument)
-            }
-        }, ContextCompat.getMainExecutor(this))
-    }
 }
