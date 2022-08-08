84ee21745e75b92102991f578a5117dd59bec68f
TimPushkin <pushkin.td@gmail.com>
2022-08-08 17:52:42 +0300

    Account for custom strides in image planes

--- app/src/main/java/com/github/kpdandroid/utils/ImageTransformations.kt
+++ app/src/main/java/com/github/kpdandroid/utils/ImageTransformations.kt
@@ -10,17 +10,43 @@ private const val RGB_CHANNELS_NUM = 3
  * Converts an array of RGBA pixel data into an array of RGB pixel data.
  *
  * @param rgbaBytes RGBA pixels in format `[R_1, G_1, B_1, A_1, ..., R_n, G_n, B_n, A_n]`.
+ * @param width width of the image.
+ * @param height height of the image.
+ * @param rowStride size of each row in bytes.
+ * @param pixelStride size of each pixel in bytes.
  * @return RGB pixels in format `[R_1, G_1, B_1, ..., R_n, G_n, B_n]`.
+ *
+ * @throws IllegalArgumentException if rowStride is less than (image width) * (pixel stride) or
+ * pixelStride is less than the number of channels in RGBA pixel (i.e. less than 4).
  */
-fun rgbaBytesToRgbBytes(rgbaBytes: ByteArray): ByteArray {
-    val pixelsNum = rgbaBytes.size / RGBA_CHANNELS_NUM
-    val rgbBytes = ByteArray(pixelsNum * RGB_CHANNELS_NUM)
-    for (pixelIndex in 0 until pixelsNum) {
-        for (channelIndex in 0 until RGB_CHANNELS_NUM) {
-            rgbBytes[RGB_CHANNELS_NUM * pixelIndex + channelIndex] =
-                rgbaBytes[RGBA_CHANNELS_NUM * pixelIndex + channelIndex]
+fun rgbaBytesToRgbBytes(
+    rgbaBytes: ByteArray,
+    width: Int,
+    height: Int,
+    rowStride: Int,
+    pixelStride: Int
+): ByteArray {
+    if (pixelStride < RGBA_CHANNELS_NUM) {
+        throw IllegalArgumentException(
+            "Pixel stride $pixelStride is less than pixel size $RGBA_CHANNELS_NUM."
+        )
+    }
+    if (rowStride < width * pixelStride) {
+        throw IllegalArgumentException(
+            "Row stride $rowStride is less than width * pixel stride = ${width * pixelStride}."
+        )
+    }
+
+    val rgbBytes = ByteArray(width * height * RGB_CHANNELS_NUM)
+    for (rowIndex in 0 until height) {
+        for (pixelIndex in 0 until width) {
+            for (channelIndex in 0 until RGB_CHANNELS_NUM) {
+                rgbBytes[RGB_CHANNELS_NUM * (width * rowIndex + pixelIndex) + channelIndex] =
+                    rgbaBytes[rowStride * rowIndex + pixelStride * pixelIndex + channelIndex]
+            }
         }
     }
+
     return rgbBytes
 }
 
@@ -42,20 +68,46 @@ private const val MAX_COLOR = 0xff // Maximum ARGB_8888 channel value
  * @param rgbaBytes RGBA pixels in format `[R_1, G_1, B_1, A_1, ..., R_n, G_n, B_n, A_n]`.
  * @param width width of the image.
  * @param height height of the image.
+ * @param rowStride size of each row in bytes.
+ * @param pixelStride size of each pixel in bytes.
  * @return mutable [Bitmap] in [Bitmap.Config.ARGB_8888] format containing the given pixels.
+ *
+ * @throws IllegalArgumentException if rowStride is less than (image width) * (pixel stride) or
+ * pixelStride is less than the number of channels in RGBA pixel (i.e. less than 4).
  */
-fun rgbaBytesToBitmap(rgbaBytes: ByteArray, width: Int, height: Int): Bitmap {
-    val buffer = IntBuffer.allocate(rgbaBytes.size / RGBA_CHANNELS_NUM).apply {
-        for (pixelIndex in rgbaBytes.indices step RGBA_CHANNELS_NUM) {
-            val r = rgbaBytes[pixelIndex + RED_POS].toInt() and MAX_COLOR
-            val g = rgbaBytes[pixelIndex + GREEN_POS].toInt() and MAX_COLOR
-            val b = rgbaBytes[pixelIndex + BLUE_POS].toInt() and MAX_COLOR
-            val a = rgbaBytes[pixelIndex + ALPHA_POS].toInt() and MAX_COLOR
-            // ARGB_8888 pixel is stored in ABGR channel order
-            put(
-                (a shl ALPHA_BIT_SHIFT) or (b shl BLUE_BIT_SHIFT) or
-                    (g shl GREEN_BIT_SHIFT) or (r shl RED_BIT_SHIFT)
-            )
+fun rgbaBytesToBitmap(
+    rgbaBytes: ByteArray,
+    width: Int,
+    height: Int,
+    rowStride: Int,
+    pixelStride: Int
+): Bitmap {
+    if (pixelStride < RGBA_CHANNELS_NUM) {
+        throw IllegalArgumentException(
+            "Pixel stride $pixelStride is less than pixel size $RGBA_CHANNELS_NUM."
+        )
+    }
+    if (rowStride < width * pixelStride) {
+        throw IllegalArgumentException(
+            "Row stride $rowStride is less than width * pixel stride = ${width * pixelStride}."
+        )
+    }
+
+    val usefulRowSize = width * pixelStride
+
+    val buffer = IntBuffer.allocate(width * height * RGBA_CHANNELS_NUM).apply {
+        for (rowShift in rgbaBytes.indices step rowStride) {
+            for (pixelShift in 0 until usefulRowSize step pixelStride) {
+                val r = rgbaBytes[rowShift + pixelShift + RED_POS].toInt() and MAX_COLOR
+                val g = rgbaBytes[rowShift + pixelShift + GREEN_POS].toInt() and MAX_COLOR
+                val b = rgbaBytes[rowShift + pixelShift + BLUE_POS].toInt() and MAX_COLOR
+                val a = rgbaBytes[rowShift + pixelShift + ALPHA_POS].toInt() and MAX_COLOR
+                // ARGB_8888 pixel is stored in ABGR channel order
+                put(
+                    (a shl ALPHA_BIT_SHIFT) or (b shl BLUE_BIT_SHIFT) or
+                        (g shl GREEN_BIT_SHIFT) or (r shl RED_BIT_SHIFT)
+                )
+            }
         }
         rewind()
     }
