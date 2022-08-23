1410ed5cc46006ebca9fa45556946334d074aa19
TimPushkin <pushkin.td@gmail.com>
2022-08-23 20:04:04 +0300

    Add native logging

--- lib/src/main/cpp/src/keypoint_detector.cpp
+++ lib/src/main/cpp/src/keypoint_detector.cpp
@@ -1,19 +1,25 @@
 #include <utility>
 #include <opencv2/imgproc.hpp>
+#include "logging.h"
 #include "conversions.h"
 #include "keypoint_detector.h"
 
 namespace kpdlib {
 
+constexpr auto kTag = "KeypointDetector";
+
 KeypointDetector::KeypointDetector(int width, int height, cv::Ptr<cv::Feature2D> detector)
     : width_(width), height_(height), detector_(std::move(detector)) {}
 
 std::shared_ptr<DetectionResult> KeypointDetector::detect(const std::vector<std::uint8_t> &pixel_data) const {
-  if (height_ <= 0 || width_ <= 0) {  // cv::cvtColor throws on empty input
+  if (width_ <= 0 || height_ <= 0) {  // cv::cvtColor throws on empty input
+    logW(kTag, "Current size is negative (%d x %d), no detection is possible.", width_, height_);
     return std::make_shared<DetectionResult>(
         std::vector<std::shared_ptr<StrippedKeypoint>>(),
         std::vector<std::vector<uint8_t>>());
   }
+
+  logV(kTag, "Starting detection (current size is %d x %d).", width_, height_);
 
   auto image = vectorToMat(pixel_data, height_, width_);
   cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
@@ -24,6 +30,8 @@ std::shared_ptr<DetectionResult> KeypointDetector::detect(const std::vector<std:
 
   auto keypoints = stripKeypoints(cv_keypoints);
   auto descriptors = matTo2DVector(cv_descriptors);
+
+  logV(kTag, "Detected %lu keypoints (and %lu descriptors).", keypoints.size(), descriptors.size());
 
   return std::make_shared<DetectionResult>(keypoints, descriptors);
 }
---
+++ lib/src/main/cpp/src/logging.h
@@ -0,0 +1,14 @@
+#ifndef LIB_SRC_MAIN_CPP_SRC_LOGGING_H_
+#define LIB_SRC_MAIN_CPP_SRC_LOGGING_H_
+
+void logV(const char *tag, const char *format...);
+
+void logD(const char *tag, const char *format...);
+
+void logI(const char *tag, const char *format...);
+
+void logW(const char *tag, const char *format...);
+
+void logE(const char *tag, const char *format...);
+
+#endif  // LIB_SRC_MAIN_CPP_SRC_LOGGING_H_
