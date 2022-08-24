73adfdf06b6b6bbbbad83a7f791bba38473d7b6c
TimPushkin <pushkin.td@gmail.com>
2022-08-24 17:46:04 +0300

    Implement algorithm choosing in native example and make its output more readable

--- lib/src/main/cpp/examples/detection_example.cpp
+++ lib/src/main/cpp/examples/detection_example.cpp
@@ -5,6 +5,27 @@
 #include "surf_detector.h"
 #include "orb_detector.h"
 
+constexpr int CLI_ARGS_NUM = 3;
+constexpr int CLI_ALGO_ARG = 1;
+constexpr int CLI_PATH_ARG = 2;
+
+// Returns a detector by its name.
+std::unique_ptr<kpdlib::KeypointDetector> parseAlgoName(const std::string &name) {
+  if (name == "sift") {
+    std::cout << "Using SIFT." << std::endl;
+    return std::make_unique<kpdlib::SiftDetector>(0, 0);
+  }
+  if (name == "surf") {
+    std::cout << "Using SURF." << std::endl;
+    return std::make_unique<kpdlib::SurfDetector>(0, 0);
+  }
+  if (name == "orb") {
+    std::cout << "Using ORB." << std::endl;
+    return std::make_unique<kpdlib::OrbDetector>(0, 0);
+  }
+  return nullptr;
+}
+
 // Converts a matrix of bytes to a 1D vector.
 std::vector<uint8_t> matToVector(const cv::Mat &mat) {
   std::vector<uint8_t> result;
@@ -13,55 +34,57 @@ std::vector<uint8_t> matToVector(const cv::Mat &mat) {
   return result;
 }
 
-// Pretty-prints keypoints and descriptors to `stdout`.
-void printOutput(const std::shared_ptr<kpdlib::DetectionResult> &output) {
-  std::cout << "Keypoints" << std::endl;
-  for (auto &kp : output->getKeypoints()) {
-    std::cout << kp->getX() << "\t" << kp->getY() << "\t" << kp->getSize() << "\t" << kp->getAngle() << "\t"
-              << kp->getStrength() << std::endl;
-  }
+// Pretty-prints detection result to `stdout`.
+void printOutput(const std::vector<std::shared_ptr<kpdlib::StrippedKeypoint>> &keypoints,
+                 const std::vector<std::vector<uint8_t>> &descriptors) {
+  for (std::size_t i = 0; i < keypoints.size(); i++) {
+    const auto &keypoint = keypoints[i];
+    const auto &descriptor = descriptors[i];
 
-  std::cout << "Descriptors" << std::endl;
-  for (const auto &row : output->getDescriptors()) {
-    for (const auto &element : row) {
-      std::cout << element << ' ';
-    }
-    std::cout << std::endl;
+    std::cout << "(" << keypoint->getX() << ", " << keypoint->getY() << ")" << "\t"
+              << "size=" << keypoint->getSize() << "\t"
+              << "angle=" << keypoint->getAngle() << "\t"
+              << "strength=" << keypoint->getStrength() << "\t"
+              << "(descriptor size is " << descriptor.size() << ")" << std::endl;
   }
 }
 
-// Calculates keypoints and image descriptors and prints them to stdout. Image file path in format compatible with
-// `imread` from OpenCV is expected as a CLI argument.
+// Calculates keypoints and image descriptors and prints them to stdout.
+//
+// Expects the following CLI arguments:
+// 1. Algorithm name (sift, surf, or orb).
+// 2. Path to an image file in format compatible with `imread` from OpenCV.
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Illegal CLI arguments num. Expected only a file path but was " << argc
-              << " arguments (including the command name)" << std::endl;
+  if (argc != CLI_ARGS_NUM) {
+    std::cerr << "Unexpected CLI arguments number: expecting algorithm name and image file path." << std::endl;
     return 1;
   }
 
-  // Read the image
-  auto *image_path = argv[1];
+  // Instantiate a detector
+  auto detector = parseAlgoName(argv[CLI_ALGO_ARG]);
+  if (detector == nullptr) {
+    std::cerr << "Unknown algorithm name: supported algorithm names are sift, surf, and orb.";
+    return 1;
+  }
+
+  // Obtain an image
+  auto *image_path = argv[CLI_PATH_ARG];
   auto img = cv::imread(image_path, cv::IMREAD_COLOR);
 
-  // Convert the image into the suitable format
+  // Convert the image into the supported format
   auto image_data = matToVector(img);
   auto width = img.cols;
   auto height = img.rows;
 
-  // Detect with SIFT
-  kpdlib::SiftDetector sift_detector(width, height);
-  auto sift_output = sift_detector.detect(image_data);
-  printOutput(sift_output);
+  // Pass image size to the detector
+  detector->setWidth(width);
+  detector->setHeight(height);
 
-  // Detect with SURF
-  kpdlib::SurfDetector surf_detector(width, height);
-  auto surf_output = surf_detector.detect(image_data);
-  printOutput(surf_output);
+  // Run detection
+  auto output = detector->detect(image_data);
 
-  // Detect with ORB
-  kpdlib::OrbDetector orb_detector(width, height);
-  auto orb_output = orb_detector.detect(image_data);
-  printOutput(orb_output);
+  // Use the resulting keypoints and descriptors as needed
+  printOutput(output->getKeypoints(), output->getDescriptors());
 
   return 0;
 }
